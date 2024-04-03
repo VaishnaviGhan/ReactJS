0d433868cf5c8c010540c1b23193c2d6fa628369
Vaishnavi <ghanvaishnavi@gmail.com>
2024-04-03 15:58:50 +0530

    I learn state of react js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function TextForm(props) {
+    const handleupClick = ()=>{
+    let newtext = text.toUpperCase();
+    setText(newtext)
+    }
+
+    const handleOnChange = (event)=>{
+      setText(event.target.value);
+      console.log('dwq')
+    }
+    const[text,setText] = useState("Enter text here")
   return (
     <>
     <div>
     <div className="mb-3">
     <label for="myBox" class="form-label">{props.heading}</label>
-    <textarea className="form-control" id="myBox" rows="8"></textarea>
+    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
     </div>
-    <button className="btn btn-primary">Convert to UpperCase</button>
+    <button className="btn btn-primary" onClick={handleupClick}>Convert to UpperCase</button>
     </div>
     </>
   
