91cf457fd3351c28f29edf58575bb75f0598ad6f
Vaishnavi <ghanvaishnavi@gmail.com>
2024-04-04 10:41:12 +0530

    we can change background red/white and button name also aacording to backgroun

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+// import About from './components/About';
 function App() {
   return (
 <>
 <Navbar title="Govinda Darshan" aboutText="Shift"/>
 <div className="container">
 <TextForm heading="Enter your Text here"/>
+{/* <About/> */}
 </div>
 
 </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,20 +5,58 @@ export default function TextForm(props) {
     let newtext = text.toUpperCase();
     setText(newtext)
     }
+    const handleClearClick = ()=>{
+        let newtext = '';
+        setText(newtext)
+        }
+
+    // const handleredClick = ()=>{
+    //     let newtext = document.getElementById('myBox')
+    //     newtext.style.backgroundColor = 'red';
+    //     }
+    const [mage,SetMage] = useState({
+        color : 'black',
+        backgroundColor:'red'
+    })
+    const [btn,Setbtn] = useState('Red Background')
+
+    const mood = () => {
+      let newtext = document.getElementById('myBox');
+      if (mage.color === 'black') {
+          newtext.style.backgroundColor = 'red';
+          newtext.style.color = 'white';
+          SetMage({backgroundColor: 'red', color: 'white' });
+          Setbtn('White Background');
+      } else {
+          newtext.style.color = 'black';
+          newtext.style.backgroundColor = 'white';
+          SetMage({backgroundColor: 'white', color: 'black' });
+          Setbtn('Red Background');
+      }
+  }
 
     const handleOnChange = (event)=>{
       setText(event.target.value);
       console.log('dwq')
     }
-    const[text,setText] = useState("Enter text here")
+    const[text,setText] = useState("")
   return (
     <>
-    <div>
+    <div className="container">
     <div className="mb-3">
     <label for="myBox" class="form-label">{props.heading}</label>
     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
     </div>
-    <button className="btn btn-primary" onClick={handleupClick}>Convert to UpperCase</button>
+    <button className="btn btn-primary mx-1" onClick={handleupClick}>Convert to UpperCase</button>
+    <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear text</button>
+    <button className="btn btn-primary mx-1" onClick={mood}>{btn}</button>
+    </div>
+    <div className="container my-5">
+        <h1>Your Text Summary</h1>
+        <p>{text.split(" ").length}words and {text.length} charachter</p>
+        <p>{0.5 * text.split(" ").length} Time required</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
     </div>
     </>
   
