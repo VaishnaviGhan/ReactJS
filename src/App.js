b2cf547ad5f6d22f82b9e08efdd27cbaaaa4f172
Vaishnavi <ghanvaishnavi@gmail.com>
2024-04-04 17:18:39 +0530

    Alert added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,39 @@ import React,{ useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 // import About from './components/About';
 function App() {
   const[mode,SetMode] = useState('light');
+  const[alert,SetAlert] = useState(null)
+  const ShowAlert = (message,type)=>{
+    SetAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      SetAlert(null);
+    }, 2000);
+
+  }
   const toggleMode = ()=>{
     if(mode === 'light'){
       SetMode('dark');
+      ShowAlert("Dark Mode has been enabled","success");
       document.body.style.backgroundColor = '#203b7a';
     }
     else{
       SetMode('light')
+      ShowAlert("Dark Mode has been enabled","success");
       document.body.style.backgroundColor = 'white';
     }
   }
   return (
 <>
 <Navbar title="Govinda Darshan" aboutText="Shift" mode={mode} toggleMode={toggleMode}/>
+<Alert alert={alert}/>
 <div className="container">
-<TextForm heading="Enter your Text here" mode={mode} />
+<TextForm  ShowAlert={ShowAlert} heading="Enter your Text here" mode={mode} />
 {/* <About/> */}
 </div>
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,6 +3,7 @@ import React,{useState} from 'react'
 export default function TextForm(props) {
     const handleupClick = ()=>{
     let newtext = text.toUpperCase();
+    props.ShowAlert("Converted to uppercase","success");
     setText(newtext)
     }
     const handleClearClick = ()=>{
@@ -42,17 +43,17 @@ export default function TextForm(props) {
     const[text,setText] = useState("")
   return (
     <>
-    <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
+    <div className="container" style={{color:props.mode==='dark'?'white':'#203b7a'}}>
     <div className="mb-3">
       <h1>My name is Ghan</h1>
     <label for="myBox" class="form-label">{props.heading}</label>
-    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
+    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'#203b7a'}} id="myBox" rows="8"></textarea>
     </div>
     <button className="btn btn-primary mx-1" onClick={handleupClick}>Convert to UpperCase</button>
     <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear text</button>
     <button className="btn btn-primary mx-1" onClick={mood}>{btn}</button>
     </div>
-    <div className="container my-5" style={{color:props.mode==='dark'?'white':'black'}}>
+    <div className="container my-5" style={{color:props.mode==='dark'?'white':'#203b7a'}}>
         <h1>Your Text Summary</h1>
         <p>{text.split(" ").length}words and {text.length} charachter</p>
         <p>{0.5 * text.split(" ").length} Time required</p>
